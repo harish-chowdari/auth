97d08d7c41bb120adce0cb3c3696e20a7c66df85
harish-nagubadi <Harish.N@veots.com>
2025-07-14 19:41:14 +0530

    constants update

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,6 +8,7 @@ dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 module.exports = {
     PORT: process.env.PORT || 2000,
     DB_URI: process.env.MONGO_URI,
+    
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
     REFRESH_SECRET: process.env.REFRESH_SECRET,
